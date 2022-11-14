13428365fec78c0751275a426ca1c1e3a4708efa
Vildana Suta <vildana.suta@edu.fit.ba>
2022-11-14 13:27:25 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Iz date riječi je moguće formirati riječ BALLOON " +
-                Solution.solution("BalloonxBalloonxBalloonxBaLon")+" puta.");
+                Solution.solution("BALLOONXYZBALLOONLABON")+"x.");
     }
 }
