9a7e4320e8adfe8aa2cbaff6b37a296c06a41bf4
Vildana Suta <vildana.suta@edu.fit.ba>
2022-11-15 22:02:26 +0100

    Exceptions branch commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,40 @@
+import java.io.*;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
+    static Logger logger = Logger.getLogger(Main.class.getName());
     public static void main(String[] args) {
-        System.out.println("Unesite riječ: ");
-        Scanner myObj = new Scanner(System.in);
-        String S = myObj.nextLine();
+        try{
+            System.out.println("Type in the word you are looking for: ");
+            Scanner inputWord=new Scanner(System.in);
+            String inputtedWord=inputWord.nextLine();
+            for(int i=0;i<inputtedWord.length();i++){
+                if(Character.isLowerCase(inputtedWord.charAt(i)))
+                    throw new NotUppercaseException("Inputted word is not UPPERCASE.");
+            }
+            File obj=new File("src\\inText.txt");
+            Scanner reader=new Scanner(obj);
+            FileWriter fileWriter=new FileWriter("src\\outText.txt");
 
-        System.out.println("Iz date riječi je moguće formirati riječ BALLOON " +
-                Solution.solution(S)+"x.");
+            while(reader.hasNextLine()) {
+              String S = reader.nextLine();
+              fileWriter.write("From the word given, inputted word can be formed " +
+                      Solution.solution(inputtedWord,S)+"x. ");
+              fileWriter.write(System.getProperty( "line.separator" ));
+            }
+            fileWriter.close();
+        }
+        catch (FileNotFoundException fnfe){ //FileNotFound-checked exception
+            logger.log(Level.WARNING,"File not found:"+fnfe.getMessage());
+        } catch (IOException | ArrayIndexOutOfBoundsException e) { //example of combining exceptions under one catch (IOException-checked, ArrayIndexOutOfBoundsException-unchecked)
+            logger.log(Level.WARNING,e.getMessage());
+        } catch(NullPointerException e){ //NullPointerException-unchecked exception
+            logger.log(Level.WARNING,e.getMessage());
+        } catch (NotUppercaseException e) { //Custom made exception
+            logger.log(Level.WARNING,e.getMessage());
+        }
     }
+
 }
