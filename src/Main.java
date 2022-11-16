a35de0eb58caf23ad899937dd4c961b822a5392a
Vildana Suta <vildana.suta@edu.fit.ba>
2022-11-16 09:44:59 +0100

    Exceptions branch commit

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,9 @@ public class Main {
             System.out.println("Type in the word you are looking for: ");
             Scanner inputWord=new Scanner(System.in);
             String inputtedWord=inputWord.nextLine();
+            if(inputtedWord.length()>200000){
+                throw new WordTooLong("Inputted word is too long. ");
+            }
             for(int i=0;i<inputtedWord.length();i++){
                 if(Character.isLowerCase(inputtedWord.charAt(i)))
                     throw new NotUppercaseException("Inputted word is not UPPERCASE.");
@@ -32,7 +35,9 @@ public class Main {
             logger.log(Level.WARNING,e.getMessage());
         } catch(NullPointerException e){ //NullPointerException-unchecked exception
             logger.log(Level.WARNING,e.getMessage());
-        } catch (NotUppercaseException e) { //Custom made exception
+        } catch (NotUppercaseException e) { //Custom made exceptions: NotUppercaseException and WordTooLong
+            logger.log(Level.WARNING,e.getMessage());
+        } catch (WordTooLong e){
             logger.log(Level.WARNING,e.getMessage());
         }
     }
